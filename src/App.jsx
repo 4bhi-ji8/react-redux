c2e0d2b7ad10376d2a6cdeee62db28fdde61bd1f
Abhijith <Abhijithchenthatta@gmail.com>
2025-11-20 12:08:21 +0530

    Added basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Contact from "./pages/Contact";
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/react-redux">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
